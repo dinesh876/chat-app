c710fb576c8ec488176606420b658e3173a8f931
dinesh876 <dineshadventure15@gmail.com>
2018-05-27 23:44:58 -1000

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+var socket=io();
+socket.on('connect',function(){
+     console.log('connected to server');
+     socket.emit('createMessage',{
+         "from":"dinesh",
+         "text":"can we meet up at 6 p.m"
+     })
+})
+socket.on('disconnect',function(){
+    console.log("server went down!!!");
+})
+socket.on('newMessage',function(message){
+    console.log("newMessage",message);
+})
